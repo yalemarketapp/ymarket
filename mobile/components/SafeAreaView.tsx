0f38fa3d996d00235940035f0c75278372c12644
Austin <austin12chen@gmail.com>
2022-07-28 22:19:02 -0700

    SafeAreaView component

--- mobile/components/SafeAreaView.tsx
+++ mobile/components/SafeAreaView.tsx
@@ -1,8 +1,17 @@
 import { FC } from 'react'
+import { ViewProps, StyleSheet } from 'react-native'
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context'
 
-const SafeAreaView: FC = ({ children }) => {
-  return <RNSafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>{children}</RNSafeAreaView>
+const SafeAreaView: FC<ViewProps> = ({ children, ...props }) => {
+  return (
+    <RNSafeAreaView {...props} style={[styles.view, props.style]}>
+      {children}
+    </RNSafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  view: { flex: 1, backgroundColor: 'white' },
+})
 
 export default SafeAreaView

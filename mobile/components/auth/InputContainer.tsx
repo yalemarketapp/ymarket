aeac83bc50bacc84aa3342bdb3372741f32ea38e
Austin <austin12chen@gmail.com>
2022-08-03 02:03:44 -0700

    components

---
+++ mobile/components/auth/InputContainer.tsx
@@ -0,0 +1,79 @@
+import React, { FC } from 'react'
+import { NativeSyntheticEvent, StyleSheet, TextInputEndEditingEventData, View } from 'react-native'
+import { formInput } from '../../types'
+import { TextInput } from '../building-blocks'
+
+type InputContainerProps = {
+  inputs: InputProps[]
+}
+
+const InputContainer: FC<InputContainerProps> = ({ inputs }) => {
+  return (
+    <>
+      <View style={styles.formContainer}>
+        {inputs.map(({ label, state, setState, validate, type }) => (
+          <Input key={label} label={label} state={state} setState={setState} validate={validate} type={type} />
+        ))}
+      </View>
+    </>
+  )
+}
+
+export type InputProps = {
+  label: string
+  state: formInput
+  setState: (state: formInput) => void
+  validate?: (value: string) => string
+  type?: 'email' | 'password' | undefined
+}
+
+const Input: FC<InputProps> = ({ label, state, setState, validate, type }) => {
+  const { value, error } = state
+
+  const emailProps = {
+    autoCapitalize: 'none',
+    autoCorrect: false,
+  }
+  const passwordProps = {
+    ...emailProps,
+    secureTextEntry: true,
+  }
+  const extraProps = type === 'email' ? emailProps : type === 'password' ? passwordProps : {}
+
+  const onEndEditing = validate
+    ? (event: NativeSyntheticEvent<TextInputEndEditingEventData>) => {
+        const error = validate(event.nativeEvent.text)
+        setState({ ...state, error })
+      }
+    : undefined
+
+  return (
+    <TextInput
+      {...extraProps}
+      style={styles.input}
+      label={label}
+      value={value}
+      onEndEditing={onEndEditing}
+      onChangeText={(value) => setState({ ...state, value })}
+      error={error}
+      margin={styles.outerInput}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  formContainer: {
+    width: '80%',
+    justifyContent: 'center',
+  },
+  input: {
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    fontSize: 16,
+  },
+  outerInput: {
+    marginVertical: 5,
+  },
+})
+
+export default InputContainer

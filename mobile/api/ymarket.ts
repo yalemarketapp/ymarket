fa90e4f080d6c1498e592c29511d5cb852aa7623
Austin <austin12chen@gmail.com>
2022-08-03 02:03:36 -0700

    ymarket api

---
+++ mobile/api/ymarket.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const ymarket = axios.create({
+  baseURL: 'http://127.0.0.1:8000', // TODO: https://linear.app/ymarket/issue/YMA-16/use-env-variables-with-backend-settingspy-and-axios-url
+  headers: {
+    'Content-type': 'application/json',
+  },
+})
+
+// Interceptors are handled in YMarket.tsx
+
+export default ymarket
